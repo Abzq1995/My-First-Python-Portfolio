2d5b0a993b841292093dad2d91c00ca17109719d
Abzq1995 <124204438+Abzq1995@users.noreply.github.com>
2023-02-03 21:57:42 +0000

    Create Madlibsgame_Project2.py

---
+++ Madlibsgame_Project2.py
@@ -0,0 +1,8 @@
+''' this game can be extended with more phrases and inputs '''
+colour = input("Enter colour: ")
+plural_noun = input("Enter Plural Noun: ")
+celebrity = input("Enter Celebrity: ")
+
+print("Roses are " + colour)
+print( plural_noun + " are blue ")
+print("I love " + celebrity)
