6e1dbbffb3862167004f5e4400a5cd6f8bd04031
Abzq1995 <124204438+Abzq1995@users.noreply.github.com>
2023-02-03 21:37:09 +0000

    Create ImprovedCalculator_Project2.py
    A simple but improved calculator from the Basic Calculator

---
+++ ImprovedCalculator_Project2.py
@@ -0,0 +1,16 @@
+''' The Variable OP consists of + , - , / , * ''' 
+
+num1 = float(input("Enter first number: "))
+OP = input("Enter first operator: ")
+num2 = float(input("Enter second number: "))
+
+if OP == "+":
+    print(num1 + num2)
+elif OP == "-":
+    print(num1 - num2)
+elif OP == "/":
+    print(num1 / num2)
+elif OP == "*":
+    print(num1 * num2)
+else:
+    print("Invalid Operator")
